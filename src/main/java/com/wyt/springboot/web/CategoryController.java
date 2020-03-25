c8595808377ae180e0d0a333cb0868e3f75f864f
wf <2661159674@qq.com>
2020-03-25 16:27:59 +0800

    learn springboot

--- src/main/java/com/wyt/springboot/web/CategoryController.java
+++ src/main/java/com/wyt/springboot/web/CategoryController.java
@@ -3,9 +3,14 @@ package com.wyt.springboot.web;
 import com.wyt.springboot.dao.CategoryDao;
 import com.wyt.springboot.pojo.Category;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -16,9 +21,41 @@ public class CategoryController {
     CategoryDao categoryDao;
 
     @RequestMapping("/categoryList")
-    public String categoryList(Model m){
+    public String categoryList(Model m) throws Exception {
         List<Category> list = categoryDao.findAll();
         m.addAttribute("list",list);
         return "categoryList";
+    }
+
+    @RequestMapping("/addCategory")
+    public String addCategory(Category c) throws Exception {
+        categoryDao.save(c);
+        return "redirect:listCategory";
+    }
+    @RequestMapping("/deleteCategory")
+    public String deleteCategory(Category c) throws Exception {
+        categoryDao.delete(c);
+        return "redirect:listCategory";
+    }
+    @RequestMapping("/updateCategory")
+    public String updateCategory(Category c) throws Exception {
+        categoryDao.save(c);
+        return "redirect:listCategory";
+    }
+    @RequestMapping("/editCategory")
+    public String editCategory(int id,Model m) throws Exception {
+        Category c= categoryDao.getOne(id);
+        m.addAttribute("c", c);
+        return "editCategory";
+    }
+
+    @RequestMapping("/listCategory")
+    public String listCategory(Model m, @RequestParam(value = "start", defaultValue = "0") int start, @RequestParam(value = "size", defaultValue = "5") int size) throws Exception {
+        start = start<0?0:start;
+        Sort sort = new Sort(Sort.Direction.DESC, "id");
+        Pageable pageable = new PageRequest(start, size, sort);
+        Page<Category> page =categoryDao.findAll(pageable);
+        m.addAttribute("page", page);
+        return "listCategory";
     }
 }
